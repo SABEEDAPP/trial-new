f6851f6e8832995dcbb0140a62c42f642670aa38
SABEEDAPP <ourbabysn1@gmail.com>
2023-03-02 19:28:01 +0300

    Annu

---
+++ INTEX/two.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Simple{
+    public sta
+            
+
+        int age; 
+        String n
+        System.out.p
+        name=sc.nextLine();
+        Syst e m.out.println(
+        }  
+    
